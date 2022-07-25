d11013140ed9ffeb4bc56611ff830e58715e7d4f
hhundley <65366145+hhundley@users.noreply.github.com>
2022-07-25 14:43:01 -0500

    connecting server-side API

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+// api request URL variables
+var oneCallRequest = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=128131299a05367570bdc9c94f118fc8';
+var geocodingRequest = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=128131299a05367570bdc9c94f118fc8';
